0519b91a6e82558a7de3cb40a0695be244f4ce6c
sunny <sunny.s.2106@gmail.com>
2025-05-08 23:12:33 -0400

    separated dev and prod env

---
+++ backend/backend/settings/dev.py
@@ -0,0 +1,21 @@
+from .base import *
+import os
+from pathlib import Path
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # vite frontend in dev
+]
+
+SECRET_KEY = "insecure-secret-key-for-dev-only"
---
+++ backend/backend/settings/prod.py
@@ -0,0 +1,18 @@
+from .base import *
+import os
+import dj_database_url
+
+
+DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
+
+CORS_ALLOWED_ORIGINS = [
+    "https://fitness-journal-mu.vercel.app",
+]
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split()
