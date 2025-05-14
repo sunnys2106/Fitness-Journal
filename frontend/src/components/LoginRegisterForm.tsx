7e3cf1657331eeb38b457fcf554df90d397249e5
sunny <sunny.s.2106@gmail.com>
2025-05-13 22:32:26 -0400

    form validation for login/register

--- frontend/src/components/LoginRegisterForm.tsx
+++ frontend/src/components/LoginRegisterForm.tsx
@@ -26,18 +26,20 @@ function LoginRegisterForm({
         e.preventDefault();
 
         try {
-            const payload =
-                method === "login"
-                    ? { username, password }
-                    : { username, password, email };
+            if (!showUsernameError && !showEmailError && !showPasswordError) {
+                const payload =
+                    method === "login"
+                        ? { username, password }
+                        : { username, password, email };
 
-            const res = await api.post(route, payload);
-            if (method === "login") {
-                localStorage.setItem(ACCESS_TOKEN, res.data.access);
-                localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
-                navigate("/");
-            } else {
-                navigate("/login");
+                const res = await api.post(route, payload);
+                if (method === "login") {
+                    localStorage.setItem(ACCESS_TOKEN, res.data.access);
+                    localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
+                    navigate("/");
+                } else {
+                    navigate("/login");
+                }
             }
         } catch (error: any) {
             alert(error?.response?.data?.detail || "An error occurred");
@@ -46,6 +48,22 @@ function LoginRegisterForm({
         }
     };
 
+    const [usernameTouched, setusernameTouched] = useState(false);
+    const isValidUsername = /^[A-Za-z0-9 -]{3,30}$/.test(username);
+    const showUsernameError = usernameTouched && !isValidUsername;
+
+    const [emailTouched, setemailTouched] = useState(false);
+    const isValidEmail = (
+        document.getElementById("emailInput") as HTMLInputElement
+    )?.checkValidity();
+    const showEmailError = emailTouched && !isValidEmail;
+
+    const [passwordTouched, setpasswordTouched] = useState(false);
+    const isValidPassword = (
+        document.getElementById("passwordInput") as HTMLInputElement
+    )?.checkValidity();
+    const showPasswordError = passwordTouched && !isValidPassword;
+
     return (
         <dialog id="my_modal_3" className="modal" open={true}>
             <div className="modal-box">
@@ -53,46 +71,92 @@ function LoginRegisterForm({
                     {method === "login" ? "Login" : "Register"}
                 </h3>
                 <form method="dialog" onSubmit={handleSubmit}>
-                    <label className="my-4 input input-bordered flex items-center gap-2">
+                    <label
+                        className={`input input-bordered flex items-center gap-2 ${
+                            showUsernameError ? "border-2 border-red-500" : ""
+                        }`}
+                    >
                         <div className="label">
-                            <span className="label-text">Username</span>
+                            <span className="label-text ">Username</span>
                         </div>
                         <input
-                            className="form-input"
+                            className="form-input grow"
                             type="text"
                             value={username}
+                            required
+                            onBlur={() => setusernameTouched(true)}
                             onChange={(e) => setUsername(e.target.value)}
+                            title="Only letters, numbers, spaces, or dashes. Max 30 characters."
                         />
                     </label>
-                    {method === "register" && (
-                        <label className="my-4 input input-bordered flex items-center gap-2">
-                            <div className="label">
-                                <span className="label-text">Email</span>
-                            </div>
-                            <input
-                                className="form-input"
-                                type="email"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                            />
-                        </label>
+                    {showUsernameError && (
+                        <span className="text-red-500 text-sm ml-2">
+                            Must contain only letters, numbers, spaces, or
+                            dashes. 3-30 characters
+                        </span>
                     )}
 
-                    <label className="my-4 input input-bordered flex items-center gap-2">
+                    {method === "register" && (
+                        <>
+                            <label
+                                className={`mt-5 input input-bordered flex items-center gap-2 ${
+                                    showEmailError
+                                        ? " border-2 border-red-500"
+                                        : ""
+                                }`}
+                            >
+                                <div className="label">
+                                    <span className="label-text">Email</span>
+                                </div>
+                                <input
+                                    id="emailInput"
+                                    className="form-input grow"
+                                    type="email"
+                                    required
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    onBlur={() => setemailTouched(true)}
+                                />
+                            </label>
+                            {showEmailError && (
+                                <span className="text-red-500 text-sm ml-2">
+                                    Email address is invalid
+                                </span>
+                            )}
+                        </>
+                    )}
+
+                    <label
+                        className={`mt-5 input input-bordered flex items-center gap-2 ${
+                            showPasswordError ? "border-2 border-red-500" : ""
+                        }`}
+                    >
                         <div className="label">
                             <span className="label-text">Password</span>
                         </div>
                         <input
-                            className="form-input"
+                            id="passwordInput"
+                            className="form-input grow"
                             type="password"
+                            pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{9,}$"
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
+                            required
+                            onBlur={() => setpasswordTouched(true)}
+                            title="Must be more than 8 characters, include a number, lowercase letter, uppercase letter"
                         />
                     </label>
+                    {showPasswordError && (
+                        <span className="text-red-500 text-sm ml-2">
+                            Must be over 8 characters, including number,
+                            lowercase and uppercase letter
+                        </span>
+                    )}
+
                     <div className="w-full flex justify-center">
                         {loading && <LoadingIndicator />}
                     </div>
-                    <div className="mt-4 w-full flex justify-center">
+                    <div className="mt-5  w-full flex justify-center">
                         <button
                             className="btn btn-wide btn-outline btn-info"
                             type="submit"
